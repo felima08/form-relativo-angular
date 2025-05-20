f8d6e121e7e3a93b258107dee54e77aef028eba6
Felipe Gonçalves <felps112233@gmail.com>
2025-05-20 16:19:29 -0300

    update

--- src/app/components/shared/error-msg/error-msg.component.ts
+++ src/app/components/shared/error-msg/error-msg.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AbstractControl, FormControl } from '@angular/forms';
+import { FormValidations } from '../form-validation';
 
 
 @Component({
@@ -19,4 +20,13 @@ export class ErrorMsgComponent implements OnInit {
   ngOnInit() {
   }
 
+  get errorMessage() {
+   for(const propertyName in this.control?.errors){
+    if(this.control.errors.hasOwnProperty(propertyName) && this.control.touched){
+      return FormValidations.getErrorMsg(this.mlabel, propertyName, this.control.errors[propertyName])
+    }
+   }
+   
+    return null
+  }
 }
